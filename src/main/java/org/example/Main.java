74cf4977a9c03ede708b9ca90af3b8bc5fea815e
Dmytro Klishch <klishch.dmytro@gmail.com>
2023-10-05 21:20:46 +0300

    added b

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,22 @@
 package org.example;
 
 import org.example.a.Database;
+import org.example.b.GardenApp;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Database db = new Database("tmp.txt");
+        /*Database db = new Database("tmp.txt");
 
         //db.addRecord("a", "b", "c", "+380");
         //System.out.println(db.getName("+380"));
         //System.out.println(db.getPhoneNumber("a"));
         db.addRecord("a1", "b", "c", "+380");
         db.addRecord("a", "b", "c", "+380");
-        System.out.println(db.removeRecord("a", "b", "c", "+380"));
+        System.out.println(db.removeRecord("a", "b", "c", "+380"));*/
+
+        GardenApp app = new GardenApp(20, 20, "garden.txt");
     }
 }
